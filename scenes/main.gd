d4e1a7c95aa3be97d186196351e435a2b55bc441
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 17:55:21 -0400

    Wrote out multiple demon levels

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,8 +3,7 @@ extends Node2D
 var demons_to_summon: Array[Demon]
 var current_demon = 0
 @onready var demonNameLabel: Label = $Grimoire/DemonName
-
-var rng = RandomNumberGenerator.new()
+@onready var demonDescription: Label = $Grimoire/Description
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,23 +18,17 @@ func _on_object_spawner_spawn_object(object: Variant) -> void:
 	add_child(object)
 
 func make_levels():
-	var puzzle = generate_puzzle()
-	var abaddon = Demon.new("Abaddon", "sheep demon", puzzle[0], puzzle[1])
-	demons_to_summon.append(abaddon)
+	demons_to_summon.append(Demon.new("Abaddon", "Demon of Desolation and Ruin", null, null))
+	demons_to_summon.append(Demon.new("Moloch", "Demon of Wrath and Violence", null, null))
+	demons_to_summon.append(Demon.new("Astaroth", "Demon of Vanity and Envy", null, null))
+	demons_to_summon.append(Demon.new("Belial", "Demon of Lawlessness and Corruption", null, null))
+	demons_to_summon.append(Demon.new("Lilith ", "Demon of Deception and Betrayal", null, null))
+	demons_to_summon.append(Demon.new("Leviathan", "Demon of Chaos and Destruction", null, null))
+	demons_to_summon.append(Demon.new("Beelzebub", "Demon of Gluttony and Excess", null, null))
+	demons_to_summon.append(Demon.new("Asmodeus", "Demon of Lust and Temptation", null, null))
 
 func load_demon(demon: Demon):
 	demonNameLabel.text = demon.name
+	demonDescription.text = demon.description
+	
 	$Altar.set_answers(demon.rows, demon.cols)
-
-func generate_puzzle():
-	var rows = [0,0,0,0,0]
-	var cols = [0,0,0,0,0]
-	
-	for i in range(5):
-		for j in range(5):
-			var should_place = rng.randf() > 0.5
-			if should_place:
-				rows[i]+=1
-				cols[j]+=1
-	
-	return [rows, cols]
--- scripts/demon.gd
+++ scripts/demon.gd
@@ -5,8 +5,28 @@ var description: String
 var cols: Array
 var rows: Array
 
+var rng = RandomNumberGenerator.new()
+
 func _init(name, description, cols, rows):
 	self.name = name
 	self.description = description
-	self.cols = cols
-	self.rows = rows
+	if cols != null and rows != null:
+		self.cols = cols
+		self.rows = rows
+	else:
+		var puzzle = generate_puzzle()
+		self.rows = puzzle[0]
+		self.cols = puzzle[1]
+
+func generate_puzzle():
+	var rows = [0,0,0,0,0]
+	var cols = [0,0,0,0,0]
+	
+	for i in range(5):
+		for j in range(5):
+			var should_place = rng.randf() > 0.5
+			if should_place:
+				rows[i]+=1
+				cols[j]+=1
+	
+	return [rows, cols]
