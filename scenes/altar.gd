afa64ae5e1115e9f51ca1993222c686bce2b9007
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 10:12:02 -0400

    removed extra platforms from altar

--- scenes/altar.gd
+++ scenes/altar.gd
@@ -1,10 +1,16 @@
-class_name Platform extends StaticBody2D
+extends Node2D
 
+var platform_matrix
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	return
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func populate_platform_matrix():
+	#for ()
 	pass
