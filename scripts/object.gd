29e3683d58b0d037d98d822ff958bab577f7fed0
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 09:10:27 -0400

    Tweaked drag/drop code

--- scripts/object.gd
+++ scripts/object.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var draggable = false
-var is_inside_droppable = false
 var body_ref
 var offset: Vector2
 var initial_pos
@@ -23,10 +22,7 @@ func _process(delta: float) -> void:
 	elif Input.is_action_just_released("click"):
 		Dragging.is_dragging = false
 		var tween = get_tree().create_tween()
-		if is_inside_droppable:
-			tween.tween_property(self, "global_position", body_ref.global_position, 0.2).set_ease(Tween.EASE_OUT)
-		else:
-			tween.tween_property(self, "global_position", initial_pos, 0.2).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "global_position", body_ref.global_position, 0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered() -> void:
 	if Dragging.is_dragging:
@@ -42,18 +38,12 @@ func _on_area_2d_mouse_exited() -> void:
 	draggable = false
 	scale = Vector2(1.0, 1.0)
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("droppable"):
 		return
 		
-	is_inside_droppable = true
 	body.modulate = Color(Color.WHITE, 0.7)
 	body_ref = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if not body.is_in_group("droppable"):
-		return
-	
-	is_inside_droppable = false
 	body.modulate = Color(Color.WHITE, 1.0)
