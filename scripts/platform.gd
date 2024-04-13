47dd105f299408a08464efa9b5555aaab0a3c5a2
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 08:27:05 -0400

    Make the platforms less opaque when something is being dragged

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -3,11 +3,11 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	modulate = Color(Color.MEDIUM_ORCHID, 0.7)
+	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Dragging.is_dragging:
-		visible = true
+		modulate = Color(Color.WHITE, 0.7)
 	else:
-		visible = false
+		modulate = Color(Color.WHITE, 1.0)
