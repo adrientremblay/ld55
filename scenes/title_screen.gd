37b55329b66d4127fe861a4b3f82b46c0a378c4a
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 18:44:01 -0400

    Started on slides for the title screen

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var slide = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+		match slide:
+			1:
+				$Slide1.visible = false
+				$Slide2.visible = true
+			2:
+				get_tree().change_scene_to_file("res://scenes/main.tscn")
