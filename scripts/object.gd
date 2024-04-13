09a295280e661819555b51b03c7bc31bf2d9e23e
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 08:01:54 -0400

    Basic dragging

--- scripts/object.gd
+++ scripts/object.gd
@@ -3,6 +3,7 @@ extends Node2D
 var draggable = false
 var is_inside_droppable = false
 var body_ref
+var offset: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +11,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not draggable:
+		return
+	if Input.is_action_just_pressed("click"):
+		offset = get_global_mouse_position() - global_position
+	if Input.is_action_pressed("click"):
+		global_position = get_global_mouse_position() - offset
+	elif Input.is_action_just_released("click"):
+		Dragging.is_dragging = false
+		var tween = get_tree().create_tween()
+		if is_inside_droppable:
+			tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered() -> void:
 	if Dragging.is_dragging:
@@ -28,8 +39,16 @@ func _on_area_2d_mouse_exited() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
+	if not body.is_in_group("droppable"):
+		return
+		
+	is_inside_droppable = true
+	body.modulate = Color(Color.REBECCA_PURPLE, 1)
+	body_ref = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if not body.is_in_group("droppable"):
+		return
+	
+	is_inside_droppable = false
+	body.modulate = Color(Color.REBECCA_PURPLE, 0.7)
