906fbfdce3485b073b272c703539f3af8fe1f3f1
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-13 16:32:40 -0400

    Wrote basic demon loadign code

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+var demons_to_summon: Array[Demon]
+var current_demon = 0
+@onready var demonNameLabel: Label = $Grimoire/DemonName
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	make_levels()
+	load_demon(demons_to_summon[current_demon])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,3 +15,9 @@ func _process(delta: float) -> void:
 
 func _on_object_spawner_spawn_object(object: Variant) -> void:
 	add_child(object)
+
+func make_levels():
+	demons_to_summon.append(Demon.new("Abaddon", "sheep demon", [], []))
+
+func load_demon(demon: Demon):
+	demonNameLabel.text = demon.name
---
+++ scripts/demon.gd
@@ -0,0 +1,12 @@
+class_name Demon extends Object
+
+var name: String
+var description: String
+var cols: Array
+var rows: Array
+
+func _init(name, description, cols, rows):
+	self.name = name
+	self.description = description
+	self.cols = cols
+	self.rows = rows
