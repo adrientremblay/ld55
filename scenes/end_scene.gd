728bb022f9da7bcb94fd29dc8fe81bcfe749a8c1
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-14 19:53:32 -0400

    End scene

--- scenes/end_scene.gd
+++ scenes/end_scene.gd
@@ -6,7 +6,6 @@ var slide = 1
 func _ready() -> void:
 	$Slide1.visible = true
 	$Slide2.visible = false
-	$Slide3.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,9 +14,4 @@ func _process(delta: float) -> void:
 			1:
 				$Slide1.visible = false
 				$Slide2.visible = true
-			2:
-				$Slide2.visible = false
-				$Slide3.visible = true
-			3:
-				get_tree().change_scene_to_file("res://scenes/main.tscn")
 		slide+=1
