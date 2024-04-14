f9225d39150516c8ab66663c0296c995076e218a
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-14 14:12:28 -0400

    level 3 is a puzzle with skulls

--- scripts/demon.gd
+++ scripts/demon.gd
@@ -34,19 +34,28 @@ func generate_puzzle(level):
 	var cols = [0,0,0,0,0]
 	
 	var candle_density = 0.5
+	var skull_density = 0.5
 	match level:
 		1:
 			candle_density = 0.2
+			skull_density = 0.0
 		2: 
 			candle_density = 0.4
+			skull_density = 0.0
 		3: 
 			candle_density = 0.6
+			skull_density = 0.3
 	
 	for i in range(5):
 		for j in range(5):
-			var should_place = rng.randf() < candle_density
-			if should_place:
-				rows[i]+=1
-				cols[j]+=1
+			var placed_offering_value = 0
+			
+			if rng.randf() < skull_density:
+				placed_offering_value = 2
+			elif rng.randf() < candle_density:
+				placed_offering_value = 1
+			
+			rows[i] += placed_offering_value
+			cols[j] += placed_offering_value
 	
 	return [rows, cols]
