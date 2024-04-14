aacb1684f8219228e58b5c1c4d7f4bd57f3efc85
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-14 17:05:42 -0400

    Updated demon lines

--- scripts/main.gd
+++ scripts/main.gd
@@ -22,11 +22,11 @@ func _on_object_spawner_spawn_object(object: Variant) -> void:
 	add_child(object)
 
 func make_levels():
-	demons_to_summon.append(Demon.new(1, "Abaddon", "Demon of Desolation and Ruin", null, null, ["I am Abaddon! Am I the first demon you've ever seen?", "I can tell you men bring ruin upon themselves.", "But for the secrets of wrath and violence, you must consult Moloch!"], "Drag the candles onto the altar so that the sum of all rows and columns equals the goal numbers. Each candle has a value of 1."))
-	demons_to_summon.append(Demon.new(2, "Moloch", "Demon of Wrath and Violence", null, null, ["I am Moloch! God of wrath and violence!", "I can tell you the secrets of spiteful men like you.", "You seek our knowledge for revenge perhaps?"], "Do the same as with the last level. There are more candles now."))
-	demons_to_summon.append(Demon.new(3, "Lilith", "Demon of Vanity and Envy", null, null, ["I am Lilith! "], "The next demon must be summoned using a combination of candles and skulls. Skulls have a value of 2"))
-	demons_to_summon.append(Demon.new(4, "Asmodeus", "Demon of Lust and Temptation", null, null, ["hello"], "instructions"))
-	demons_to_summon.append(Demon.new(5, "Lucifer", "Demon of Vanity and Envy", null, null, ["I am Astaroth! Write it down. You will be hearing of me a lot.", "Vanity and envy are the downfall of all men", "But will anyone remember your name when the world burns?"], "The next demon must be summoned using a combination of candles and skulls. Skulls have a value of 2"))
+	demons_to_summon.append(Demon.new(1, "Abaddon", "Demon of Desolation and Ruin", null, null, ["I am Abaddon! Am I the first demon you've ever seen?", "I can tell you why men bring ruin upon themselves. But tell me first...", "Why do you? HA!"], "Drag the candles onto the altar so that the sum of all rows and columns equals the goal numbers. Each candle has a value of 1."))
+	demons_to_summon.append(Demon.new(2, "Moloch", "Demon of Wrath and Violence", null, null, ["I am Moloch! God of wrath and violence!", "I grant you the knowledge of why men desire revenge on one condition...", "You do not seek vengence against me for telling you."], "Do the same as with the last level. There are more candles now."))
+	demons_to_summon.append(Demon.new(3, "Lilith", "Demon of Deception and Betrayal", null, null, ["I am Lilith! You know my name!", "I offer my mastery of whispers, deception, and betreyal.", "But will you trust my words mortal?"], "The next demon must be summoned using a combination of candles and skulls. Skulls have a value of 2"))
+	demons_to_summon.append(Demon.new(4, "Asmodeus", "Demon of Lust and Temptation", null, null, ["I am Asmodeus! Do you desire me?", "No! You desire knowledge.", "Dont you know knowledge is the dirtiest of all desires!"], "Do the same as with the last level. There are more skulls now."))
+	demons_to_summon.append(Demon.new(5, "Lucifer", "Lord of all other demons", null, null, ["I am Lucifer! The one who stands against the TYRANNY of God.", "I see you have already summoned and spoken to the others.", "My gift to you is the knowledge of the world beyond this one...", "You may want to stay in this one as long as you can...", "HAHAHAHA!!!!!"], "Do the same as with the last level. There are more skulls now."))
 
 func load_demon(demon: Demon, level_number: int):
 	demonNameLabel.text = demon.name
