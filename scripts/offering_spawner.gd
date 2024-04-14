2d64050c99ced394b4f93bd7d533e1f41859e1ab
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-14 00:21:26 -0400

    Make offering smaller increase slightly in sidze when candle over

--- scripts/offering_spawner.gd
+++ scripts/offering_spawner.gd
@@ -26,9 +26,11 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_mouse_entered() -> void:
 	can_spawn = true
+	scale = Vector2(5.1,5.1)
 
 func ask_altar_to_check_game_completion():
 	altar_check.emit()
 
 func _on_area_2d_mouse_exited() -> void:
 	can_spawn = false
+	scale = Vector2(5.0,5.0)
